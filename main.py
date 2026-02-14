7544107a6e7d6c3ee7d6f31239345cb506b2d6a7
Asilbek-dev309 <asilbekjon0114@gmail.com>
2026-02-14 13:41:52 +0500

    done

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
+import requests as r
+import json
+import csv
+url = 'https://www.codewars.com/users/asilbekjon0114/completed.json'
+res = r.get(url)
 
+inf = {}
+inf["username"] = res.json()["username"]
+inf["githubUsername"] = res.json()["githubUsername"]
+inf['honor'] = res.json()['honor']
+inf['clan'] = res.json()['clan']
+inf['leaderboardPosition'] = res.json()['leaderboardPosition']
+inf['authoredKata'] = res.json()['authoredKata']
+
+with open('codewars.json', 'w') as f:
+    f.write(json.dumps(inf, indent=4))
+
+
+katas=[]
+for i in res.json()['completed']:
+    katas.append(i['kata'])
+with open('katas.json','w') as f:
+    f.write(json.dumps(katas, indent=4))
+
+
+with open('katas.csv', 'w', newline='') as f:
+    writer = csv.DictWriter(f, fieldnames=katas[0].keys())
+    writer.writeheader()
+    writer.writerows(katas)
