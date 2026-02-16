f5a20ba6011aeb7fbc1b6d309ebc98bfe8ddbaa6
Asilbek-dev309 <asilbekjon0114@gmail.com>
2026-02-16 12:46:58 +0500

    done

--- making_api.py
+++ making_api.py
@@ -1,7 +1,17 @@
 import requests as r
-api_key='b159586956a84a9cb4ddbc44f540a503'
-headers={'X-Api-Key': api_key}
-url='https://randommer.io/api/Name?nameType=firstname&quantity=2'
-res=r.get(url, headers=headers)
+import os
+
+api_key = os.environ.get("RANDOMMER_API_KEY")
+
+
+headers = {
+    'X-Api-Key': api_key
+}
+
+url = 'https://randommer.io/api/Name?nameType=firstname&quantity=2'
+
+res = r.get(url, headers=headers)
+
 print(res.status_code)
 print(res.json())
+
